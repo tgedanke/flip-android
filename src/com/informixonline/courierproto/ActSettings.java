7cc31d6b78cfd90bf30189503f0fbd88d8ee42b7
Alex Ivanov <agivanov@stalcom.com>
2013-04-26 13:15:32 +0400

    FIX: files actsettings.xml and ActSettings.java not added in prev commit add in current commit

---
+++ src/com/informixonline/courierproto/ActSettings.java
@@ -0,0 +1,61 @@
+package com.informixonline.courierproto;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+/*
+ * Сохранение и чтение настроек приложения
+ */
+public class ActSettings extends Activity implements OnClickListener {
+	
+	// Save setting to SharedPref
+	final String APPCFG_LOGIN_URL = "LOGIN_URL";
+	final String APPCFG_GETDATA_URL = "GETDATA_URL";
+	SharedPreferences sharedAppConfig;
+	
+	EditText edtLOGIN_URL, edtGETDATA_URL;
+	Button btnSaveSet;
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.actsettings);
+		Log.d("ActSettings", "--- OnCreate ActSettings ---");
+		
+		edtLOGIN_URL = (EditText)findViewById(R.id.edtLOGIN_URL);
+		edtGETDATA_URL = (EditText)findViewById(R.id.edtGETDATA_URL);
+		btnSaveSet = (Button)findViewById(R.id.btnSaveSet);
+		
+		sharedAppConfig = getPreferences(MODE_PRIVATE);
+		
+		// Read prev settings
+		edtLOGIN_URL.setText(sharedAppConfig.getString(APPCFG_LOGIN_URL, ""));
+		edtGETDATA_URL.setText(sharedAppConfig.getString(APPCFG_GETDATA_URL, ""));
+		
+		btnSaveSet.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnSaveSet:
+			
+			Editor ed = sharedAppConfig.edit();
+			ed.putString(APPCFG_LOGIN_URL, edtLOGIN_URL.getText().toString());
+			ed.putString(APPCFG_GETDATA_URL, edtGETDATA_URL.getText().toString());
+			ed.commit();
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+}
